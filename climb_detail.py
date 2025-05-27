076ec7a86d73d659c1f2b61f18f1963545e21e10
je837h <je837h@tutamail.com>
2025-05-27 17:49:25 +0100

    Got climbs, grades and linksgit add .

---
+++ climb_detail.py
@@ -0,0 +1,54 @@
+grade_type_list = {"12":"Scrambling","1":"Winter","2":"Trad","3":"Sport"}
+
+grade_list = {
+    354:"Grade-1",
+    355:"Grade-2",
+    356:"Grade-3",
+    220:"I",
+    22:"VS",
+    23:"HVS",
+    20:"HS",
+    16:"VD",
+    60:"E6",
+    13:"D",
+    19:"S",
+    25:"E2",
+    568:"Project",
+    }
+
+l_grade_list = {"12":{"354":
+    {"id":354,"name":"Grade-1","gradesystem":17,"score":1,"gradecolor":'null'},"355":
+    {"id":355,"name":"Grade-2","gradesystem":17,"score":2,"gradecolor":'null'},"357":
+    {"id":357,"name":"3S","gradesystem":17,"score":4,"gradecolor":'null'},"356":
+    {"id":356,"name":"Grade-3","gradesystem":17,"score":3,"gradecolor":'null'}},"1":{"220":
+    {"id":220,"name":"I","gradesystem":1,"score":1,"gradecolor":0}},"2":{"22":
+    {"id":22,"name":"VS","gradesystem":2,"score":12,"gradecolor":1},"23":
+    {"id":23,"name":"HVS","gradesystem":2,"score":13,"gradecolor":1},"66":
+    {"id":66,"name":"none","gradesystem":2,"score":2,"gradecolor":'null'},"20":
+    {"id":20,"name":"HS","gradesystem":2,"score":10,"gradecolor":1},"16":
+    {"id":16,"name":"VD","gradesystem":2,"score":6,"gradecolor":0},"60":
+    {"id":60,"name":"E6","gradesystem":2,"score":24,"gradecolor":3},"13":
+    {"id":13,"name":"D","gradesystem":2,"score":4,"gradecolor":0},"19":
+    {"id":19,"name":"S","gradesystem":2,"score":9,"gradecolor":0},"25":
+    {"id":25,"name":"E2","gradesystem":2,"score":16,"gradecolor":2}},"3":{"568":
+    {"id":568,"name":"Project","gradesystem":6,"score":50,"gradecolor":4}}}
+
+climb_symbols = {"4":{"id":4,"name":"Reachy"},
+            "5":{"id":5,"name":"Sustained moves"},
+            "7":{"id":7,"name":"Fingery holds"},
+            "8":{"id":8,"name":"Rounded"},
+            "9":{"id":9,"name":"Loose rock may be encountered"},
+            "10":{"id":10,"name":"Sit Start"},
+            "11":{"id":11,"name":"Dyno"},
+            "12":{"id":12,"name":"Awkward moves, wide cracks"},
+            "13":{"id":13,"name":"Bold or commitiing, run-out climbing"},
+            "14":{"id":14,"name":"Powerful moves"},
+            "15":{"id":15,"name":"Technical moves"}}
+
+buttress_symbols = {"1":{"id":1,"name":"Seepage","icon_name":"fak fa-conditions-1"},
+            "2":{"id":2,"name":"Tidal","icon_name":"fak fa-conditions-2"},
+            "3":{"id":3,"name":"Often dry even when raining","icon_name":"fak fa-conditions-3"},
+            "4":{"id":4,"name":"Restricted","icon_name":"fak fa-conditions-4"},
+            "5":{"id":5,"name":"Sheltered","icon_name":"fak fa-conditions-5"},
+            "6":{"id":6,"name":"Windy","icon_name":"fak fa-conditions-6"},
+            "7":{"id":7,"name":"Abseil required","icon_name":"fak fa-conditions-7"}}
--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
-import urllib.request
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import re
+from climb_detail import grade_list
 
-# Headers to mimic the browser
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 \
     (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
@@ -15,8 +15,14 @@ book_page = requests.get('https://www.ukclimbing.com/logbook/books/#c1', headers
 soup = BeautifulSoup(book_page.content, "html.parser")
 
 book_html = soup.find(id = 'c1')
-
 tag_types = ["a"]
+
+def convert_grade(x):
+    try:
+        v = grade_list[int(x)]
+    except:
+        v = 'Unknown'
+    return v
 
 def find_crags(x):
     soup = BeautifulSoup(requests.get(x).content, 'html.parser')
@@ -29,31 +35,41 @@ def find_crags(x):
                 with open("crag names.txt", "a") as f:
                     f.writelines(names + '\n')
 
-def climbs(y):
+def climbs(y, z):
     soup = BeautifulSoup(requests.get(y).content, 'lxml')
-    table = soup.find('table', id='climb_table')
-    if table:
-        headers = [th.text.strip() for th in table.find_all('th')]
-        rows = []
-        for row in table.tbody.find_all('tr'):
-            cells = row.find_all('a')  
-            # cells = [tr.text.strip() for tr in row.find_all('td')]
-            rows.append(cells)
-        # log data
-        print("Headers:", headers)
-        print("table_body:")
-        for row in rows:
-            print(row)
+    scripts = soup.find_all('script')
 
+    for script in scripts:
+        if script.string is not None:
+            if script.string.strip().startswith('let cragId ='):
+                # find table data (list of climbs & info)
+                a = script.string.strip()
+
+                c = a.split('table_data =')[1] # remove everything before 'table_data'
+                c = c.split('grade_type_list')[0] # remove everything after 'grade_type_list'
+                c = c.lstrip(' [').rstrip('],\n\t\t\t\t')
+                
+                tokens = re.split(r"[:,{}]", c)
+
+                for i in range(len(tokens)):
+                    if tokens[i] == '"name"':
+                        name = tokens[i+1].strip('""')
+                        grade = tokens[i+3].strip('""')
+                        igrade = convert_grade(grade)
+                        print(igrade)
+                        link = tokens[i+27].strip('""')
+                        base_link = z
+                        with open("climb names.txt", "a") as f:
+                            f.write(base_link + '|' + name + '|' + igrade + '|/' + link + '\n')
+        
 for tag_type in tag_types:
     for tag in book_html.find_all(tag_type):
         logbook = tag.attrs.get('href')
         if logbook:
             full_link = 'https://www.ukclimbing.com' + logbook
-            # find_crags(full_link)
         
 with open("crag names.txt", "r") as f:
     lines = f.readlines()
     for l in lines:
         climb_full_link = base_url + l.strip('\n')
-        climbs(climb_full_link)
+        climbs(climb_full_link, l.strip())
